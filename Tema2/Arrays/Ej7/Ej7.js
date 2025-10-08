ecaa4a0e3bc0b20ba80dc3431b4d137941be4c3a
ruben <rcgfregenal@gmail.com>
2025-10-08 16:22:39 +0200

    ej7pcempezado

--- Tema2/Arrays/Ej7/Ej7.js
+++ Tema2/Arrays/Ej7/Ej7.js
@@ -1,12 +1,16 @@
 /*
-6.Escribe un script para generar un array de dos dimensiones (matriz). Se le pedirá al usuario
-primero el tamaño de la matriz (que será cuadrada):
+7. Dado un array con elementos repetidos, escribe el código para que saber cuál es el elemento que
+más veces aparece en el array.
 
- */
+UTILIZO EL ARRAY DEL EJERCICIO 6
+
+*/
+
 function crearTabla() {
   let tabla = new Array(); //Declaramos el array e instanciamos
   let dimension = parseInt(prompt('Tamaño de la matriz'));
   let rdo = '<table>'; //Empezamos en html para que se vaya creando la tabla
+  let contador = 0;
 
   //Bucle de filas
   for (let filas = 0; filas < dimension; filas++) {
@@ -29,5 +33,10 @@ function crearTabla() {
   }
 
   rdo += '</table>';
-  document.getElementById('tabla').innerHTML = rdo;
+  //Aqui recorremos la matriz (tabla) para ver todos los numeros y cuente cual se repite mas
+  for (let i = 0; i < tabla.length; i++) {
+    contador++;
+  }
+  document.getElementById('tabla').innerHTML =
+    rdo + 'El numero que mas repite en la tabla es el: ' + contador;
 }
