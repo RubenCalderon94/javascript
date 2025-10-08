f202b2cd6637c86cb87c2ebeac85878349c994c6
ruben <rcgfregenal@gmail.com>
2025-10-08 14:16:55 +0200

    ej6pcempezado

--- Tema2/Arrays/Ej6/Ej6.js
+++ Tema2/Arrays/Ej6/Ej6.js
@@ -1,21 +1,29 @@
 /*
 6.Escribe un script para generar un array de dos dimensiones (matriz). Se le pedirá al usuario
 primero el tamaño de la matriz (que será cuadrada):
+
  */
 function crearTabla() {
-  let array = new Array();
   let tabla = new Array();
-  let dimension = parseInt(prompt("Tamaño de la matriz"));
+  let dimension = parseInt(prompt('Tamaño de la matriz'));
 
-  let rdo = "<table>";
+  let rdo = '<table>';
 
+  //Bucle de filas
   for (let filas = 0; filas < dimension; filas++) {
-    for (let i = 0; i < dimension; i++) {
-      array.push(i);
+    //let array = []; //Se crea un nuevo array por cada fila.ESTO ES EN CASO PARA ALMACENAR, NO MOSTRAR AHORA
+    rdo += '<tr>'; // inicio fila
+
+    //Bucle de columnas
+    for (let col = 0; col < dimension; col++) {
+      let rellena = parseInt(prompt('Dime el valor a rellenar')); //Añadimos un valor a cada columna
+      //array.push(rellena); SERIA EN CASO DE GUARDAR NO IMPRIMIR AHORA
+      rdo += '<td>' + rellena + '</td>'; // añadimos directamente a la tabla
     }
+    rdo += '</tr>'; // fin fila
     tabla.push(array);
   }
 
-  rdo += "</table>";
-  document.getElementById("tabla").innerHTML = rdo;
+  rdo += '</table>';
+  document.getElementById('tabla').innerHTML = rdo;
 }
