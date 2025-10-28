58788a80047e44ccd7d301ae9e12c66138787bd1
ruben <rcgfregenal@gmail.com>
2025-10-28 13:00:26 +0100

    28/10pc

--- Tema2/Funciones/Actividad 8/script.js
+++ Tema2/Funciones/Actividad 8/script.js
@@ -10,37 +10,55 @@ Crea un botón “Modificar” que pida un producto a buscar y si lo encuentra m
 ejemplo, que busque “Té” y te permita modificarlo a “Té verde”.
 */
 
-function añadir() {
-  let array = ["Leche", "Café", "Té", "Miel"];
-  let productoInicio = "Carne";
-  let productoFinal = "Azucar";
+let array = ['Leche', 'Café', 'Té', 'Miel']; //Declarado global si no, no funciona correctamente
 
-  if (array.includes(productoInicio) || array.includes(productoFinal)) {
-    alert("Los productos ya existen");
+function añadir() {
+  let carne = 'Carne';
+  let azucar = 'Azúcar';
+
+  if (array.includes(azucar) || array.includes(carne)) {
+    // para que me diga si el producto ya esta añadido
+    //hay que declararlo fuera de la funcion el ARRAY
+    alert('Ya se ha añadido este producto a la lista de la compra');
     return;
   }
-
-  array.unshift("Carne");
-  array.push("Azucar");
+  array.unshift('Carne');
+  array.push('Azúcar');
 
   alert(array);
 }
 
 function alergias() {
-  let usuario = prompt("Introduce el producto que te produce alergia");
-  if (array.includes(usuario)) {
-    let posicion = array.indexOf(usuario);
-    posicion.splice(posicion, 1);
-    alert(array);
+  let usuario = prompt('Dime a que producto eres alergico');
+  usuario = usuario.trim().toLowerCase(); // limpiamos espacios y minúsculas
+
+  //CREAMOS UNA COPIA DEL ARRAY Y LO GUARDAMOS PARA GUARDAR EL ARRAY EN MINUSCULAS
+  let arrayMinus = [];
+
+  for (let producto of array) {
+    arrayMinus.push(producto.toLowerCase()); //Se añaden al nuevo array pero todas en minusculas
+  }
+
+  if (arrayMinus.includes(usuario)) {
+    let posicion = arrayMinus.indexOf(usuario); //vemos la posicion real y lo devolvemos
+    let eliminado = array.splice(posicion, 1); // guardamos el nombre real (con mayúscula, tilde, etc.)
+    // eliminamos del array original
+
+    alert('Producto eliminado ' + eliminado);
+    alert('Nueva lista: ' + array);
   } else {
-    alert("El producto no existe en la cesta");
+    alert('No existen alergias en la cesta');
   }
 }
 
 function modificar() {
-  let usuario = prompt("Dime el producto a buscar");
+  let usuario = prompt('Dime que producto quieres modificar');
   if (array.includes(usuario)) {
+    let usuar = prompt('Añadir modificacion');
+    let posicion = array.indexOf(usuario); // obtenemos la posición del producto en el array
+    array[posicion] = usuar; // modificamos el array con lo que el usuario ha introducido
+    alert('Array modificado: ' + array);
   } else {
-    alert("El producto no existe");
+    alert('No se encontró el producto en la cesta');
   }
 }
