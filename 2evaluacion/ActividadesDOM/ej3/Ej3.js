49547da7191054f0c79ec8e1afe25f85e06dbc0b
ruben <rcgfregenal@gmail.com>
2026-01-14 22:12:52 +0100

    14/01/clase

---
+++ 2evaluacion/ActividadesDOM/ej3/Ej3.js
@@ -0,0 +1,21 @@
+/* Ejercicio 6
+Dado este HTML:
+<div id="contenedor-lista"></div>
+Y este array en JavaScript:
+const ciudades = ["Madrid", "Barcelona", "Valencia", "Sevilla"];
+Tareas:
+1. Crea un título h2 que diga "Ciudades de España".
+2. Crea una lista <ul> con todos los elementos del array.
+3. Inserta todo en el div usando innerHTML.
+4. Muestra un alert que diga “Lista creada con X ciudades”*/
+
+function ejecutar() {
+
+    
+  const ciudades = ["Madrid", "Barcelona", "Valencia", "Sevilla"];
+
+  
+}
+
+
+
