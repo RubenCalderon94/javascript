2989b38a3faee3153f977b3ebd0c0396ef6d879f
ruben <rcgfregenal@gmail.com>
2026-01-16 18:11:09 +0100

    16/01/clase

--- 2evaluacion/ActividadesDOM/ej6/Ej6.js
+++ 2evaluacion/ActividadesDOM/ej6/Ej6.js
@@ -10,12 +10,46 @@ Tareas:
 4. Muestra un alert que diga “Lista creada con X ciudades”*/
 
 function ejecutar() {
+  //1. Crea un título h2 que diga "Ciudades de España".
 
-    
+  //creamos la variable donde escribimos la frase, y luego
+  //a añadir en el contenedor
+  let resultado = "<h2>Ciudades de España</h2>";
+
+  //creamos la variable donde seleccionamos el div por id  "#contenedor-lista"
+  let contenedor = document.getElementById("contenedor-lista");
+
+  //añadimos al div selecionado con id  ("#contenedor-lista") y con
+  //  (contenedor.innerHTML) estamos ubicando donde se añadira la
+  // variable resultado (que sera Ciudades de España ).
+  
+
+
+
+  // 2. Crea una lista <ul> con todos los elementos del array
   const ciudades = ["Madrid", "Barcelona", "Valencia", "Sevilla"];
 
-  
+ 
+  let mostrarCiudades = "<ul>";
+  resultado += mostrarCiudades;
+
+  for (const ciudad of ciudades) {
+    mostrarCiudades += `<li>${ciudad}</li>`;
+  }
+
+  mostrarCiudades += "</ul>";
+
+  resultado += mostrarCiudades;
+
+  //AQUI AL USAR CONTENEDOR LO QUE ESTAMOS HACIENDO ES METER DENTRO 
+  //AL SER SELECCIONADO ANTERIORMENTE, AHORA CON innerHTML = resultado
+  //lo que hacemos es añadir al resultado lo obtenido anteriormente
+  contenedor.innerHTML = resultado;
+
+  // 3.
+
+  // 4.Muestra un alert que diga “Lista creada con X ciudades”*/
+  alert("Lista creada con "+`${ciudades.length}`+ " ciudades")
+
+
 }
-
-
-
