8047c9831025007735aea1171164df793750ec7b
ruben <rcgfregenal@gmail.com>
2026-02-11 22:10:41 +0100

    11/02/clase

---
+++ 2evaluacion/Entrega Alumnos/miscript.js
@@ -0,0 +1,30 @@
+//--------------//
+//CREAMOS ARRAY//
+//-------------//
+// El array vacío
+let listaDeTareas = [];
+
+
+
+
+function anadirTarea(){
+let contadorId=0;
+// Creamos el objeto de la tarea
+let nuevaTarea = {
+    id: contadorId++,
+    descripcion: "",
+    tipo: "",
+    completada: false,
+    fechaCompletada: null
+};
+
+listaDeTareas.push(nuevaTarea);
+
+
+const formContainer = document.querySelector(".form-container");
+formContainer
+
+}
+
+
+
