6e995718c9bf5486a35528d52fff6b52cb59e732
ruben <rcgfregenal@gmail.com>
2026-03-10 19:46:56 +0100

    10/03

---
+++ 2evaluacion/ejemplo clase/miscript.js
@@ -0,0 +1,12 @@
+let arrayPrueba = [
+
+]
+
+
+arrayPrueba.forEach(function(elemento, indice){
+    const plantilla = document.querySelector("#plantilla")
+    const clon = plantilla.content.cloneNode(true)
+    clon.querySelector("#parrafo").textContent = 
+
+
+})
